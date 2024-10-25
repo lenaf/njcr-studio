bdde212c97a738e346bba4c49b20f812d0bef34e
lenaf <lforti41@gmail.com>
2024-10-25 17:59:23 -0400

    tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import background from "public/images/photography/background.jpg"
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center h-screen	bg-white">
+    <div className="relative flex flex-col items-center h-dvh	bg-white">
 
-      <section id='home' className="h-full w-full">
+      <section id='home' className="h-dvh w-full">
         <Image
           src={background}
           alt="Living room designed by Noah"
           fill
           className="absolute object-cover object-middle"
         />
-        <div className="relative h-full w-full flex flex-col items-center justify-between z-10 py-8">
+        <div className="relative h-full w-full flex flex-col items-center justify-between z-10">
           <h1>NJCR STUDIO</h1>
-          <h1>NJCRSTUDIO@GMAIL.COM</h1>
+          <h1 style={{ textDecoration: "none" }}>NJCRSTUDIO@GMAIL.COM</h1>
         </div>
       </section>
     </div>
