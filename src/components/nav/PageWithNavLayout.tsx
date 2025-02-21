516b189da773fb510e19c3ea212eed71582fdf8e
lena.forti <lena.forti@disneystreaming>
2025-02-21 12:36:45 -0500

    padding

--- src/components/nav/PageWithNavLayout.tsx
+++ src/components/nav/PageWithNavLayout.tsx
@@ -7,7 +7,7 @@ export default function PageLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex py-16 px-10 md:px-16 sm:gap-16 md:gap-24 h-screen">
+    <div className="flex p-16 sm:gap-16 md:gap-24 h-screen">
       <div className="mt-10">
         <MobileNav />
         <DesktopNav />
