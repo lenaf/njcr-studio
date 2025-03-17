77ee9beda8ad0518ebca686b383a462da2b1ee66
lena.forti <lena.forti@disneystreaming>
2025-03-16 19:58:36 -0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import '../css/index.css'
 import siteData from "@/data/siteData";
 import localFont from 'next/font/local'
 import { Analytics } from '@vercel/analytics/next';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const minion = localFont({ src: "../../public/fonts/MinionPro-Regular.otf" })
-
 
 export const metadata: Metadata = {
   title: siteData.title,
@@ -19,11 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html data-theme="light" lang="en">
+
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <main className={minion.className}>
           {children}
           <Analytics />
+          <GoogleAnalytics gaId="G-31R7KZ3VDG" />
         </main>
       </body>
     </html>
