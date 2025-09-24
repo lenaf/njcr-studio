45757ff377eea0d9d6f50c6aeeba13a15a1a625b
lena.forti <lena.forti@disneystreaming>
2025-09-24 17:55:14 -0400

    upate email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,38 @@
-import { genPageMetadata } from "../seo"
+import { genPageMetadata } from "../seo";
 import Link from "next/link";
 import ContactImage from "./ContactImage";
 
-export const metadata = genPageMetadata({ title: 'Contact' })
+export const metadata = genPageMetadata({ title: "Contact" });
 
 export default function Page() {
   return (
     <div className="md:grid md:grid-cols-2 gap-8 md:gap-16 flex flex-col h-full w-full">
       <div className="md:mt-10">
-        <div><Link className="no-underline" href="mailto: info@mariposa.gallery">njcrstudio@gmail.com</Link></div>
-        <div> <Link className="no-underline" href="tel:+1 917 209 9253"> 917.209.9253 </Link></div>
-        <div><Link className="no-underline" target="_blank" href='https://www.instagram.com/noahruttenberg/'>@noahruttenberg</Link></div>
+        <div>
+          <Link className="no-underline" href="mailto: njcrstudio@gmail.com">
+            njcrstudio@gmail.com
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <Link className="no-underline" href="tel:+1 917 209 9253">
+            {" "}
+            917.209.9253{" "}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="no-underline"
+            target="_blank"
+            href="https://www.instagram.com/noahruttenberg/"
+          >
+            @noahruttenberg
+          </Link>
+        </div>
       </div>
       <div className="relative flex-grow md:mt-10">
         <ContactImage />
       </div>
     </div>
-  )
+  );
 }
-
